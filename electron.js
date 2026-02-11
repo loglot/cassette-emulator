5a12bdacb597507b4f12b00d46f3e4701b3b9225
loglot <prattlog3@gmail.com>
2026-02-11 17:18:27 +0100

    started electron stuff

---
+++ electron.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 150,
+    height: 90,
+    frame: false,
+    transparent: true
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import { imports } from "./lib/imports.js";
 const canvas=document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 var game=new imports()
-canvas.width=500
-canvas.height=500
+canvas.width=150
+canvas.height=90
 function tick(){
     game.tape.draw(ctx)
     requestAnimationFrame(tick)

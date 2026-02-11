271b6d34aa6c64996d77b111c497b04a435ab525
loglot <88983354+loglot@users.noreply.github.com>
2026-02-11 15:57:33 +0000

    added flipping of sides

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ var game=new imports()
 canvas.width=500
 canvas.height=500
 function tick(){
+canvas.width=500
+canvas.height=500
     game.tape.draw(ctx)
     requestAnimationFrame(tick)
+    if(Math.random()*1000>998){
+        game.tape.flip()
+    }
 }
 requestAnimationFrame(tick)
